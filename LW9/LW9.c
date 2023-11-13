ee5412e1535704c6b098e3cc87abaf6e49bd5946
Вадим Романов <vadim.romanov.05@list.ru>
2023-11-13 08:16:03 +0300

    Залить окончательную версию ЛР9

--- LW9/LW9.c
+++ LW9/LW9.c
@@ -1,61 +1,5 @@
 #include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
-
-
-int max(int a, int b)
-{
-    if (a >= b)
-    {
-        return(a);
-    }
-    else
-    {
-        return(b);
-    }
-}
-
-
-int min(int a, int b)
-{
-    if (a <= b)
-    {
-        return(a);
-    }
-    else
-    {
-        return(b);
-    }
-}
-
-
-int abs(int a)
-{
-    if (a >= 0)
-    {
-        return(a);
-    }
-    else
-    {
-        return(-a);
-    }
-}
-
-
-int sign(int a)
-{
-    if (a > 0)
-    {
-        return(1);
-    }
-    else if (a = 0)
-    {
-        return(0);
-    }
-    else
-    {
-        return(-1);
-    }
-}
+#include <math.h>
 
 
 int main(){
@@ -63,13 +7,84 @@ int main(){
     i = -7;
     j = -19;
     l = 4;
-    for (k = 1; k <= (scanf("%lf", &limit)); k++) {
-        i1 = max(i * j, max(i * l, j * l)) % 30 + k;
-        j1 = abs(j - l) * sign(i) - abs(i - l) * sign(j);
-        l1 = min(i, max(j, min(l, max(i - l, j - l))));
-        i = i1;
+    scanf("%d", &limit);
+    for (k = 1; k <= limit; k++) {
+        i1 = j1 = l1 = 0;
+        if (((i * j) >= (i * l)) && ((i * j) >= (j * l)))
+        {
+            i1 = (i * j) % 30 + k;
+        }
+        else if (((i * l) >= (i * j)) && ((i * l) >= (j * l)))
+        {
+            i1 = (i * l) % 30 + k;
+        }
+        else
+        {
+            i1 = (j * l) % 30 + k;
+        }
+
+        if ((j - l) >= 0)
+        {
+            j1 = j - l; 
+        }
+        else
+        {
+            j1 = l - j;
+        }
+        if (i = 0)
+        {
+            j1 = 0;
+        }
+        else if (i < 0)
+        {
+            j1 = -j1;
+        }
+        if ((((i - l) >= 0) && (j > 0)) || (((i - l) < 0) && (j < 0)))
+        {
+            j1 = j1 - (i - l);
+        }
+        else if ((((i - l) < 0) && (j > 0)) || (((i - l) >= 0) && (j < 0)))
+        {
+            j1 = j1 - (l - i);
+        }
+
+        if ((i - l) >= (j - l))
+        {
+            l1 = i - l;
+        }
+        else
+        {
+            l1 = j - l;
+        }
+        if (l <= l1)
+        {
+            l1 = l;
+        }
+        if (j >= l1)
+        {
+            l1 = j;
+        }
+        if (i <= l1)
+        {
+            l1 = i;
+        }
+    
         j = j1;
         l = l1;
-        printf("%d", k);
+        i = i1;
+        printf("%s ",  "iteration:");
+        printf("%d\n", k);
+        printf("i = %d; ", i);
+        printf("j = %d; ", j);
+        printf("l = %d. \n", l);
+
+        if ((pow(0.5 * (i - 20), 2) + pow(j, 2)) <= 25)
+        {
+            printf("%s. \n", "Coordinats are in ellipse");
+        }
+        else
+        {
+            printf("%s. \n", "Coordinats are not in ellipse");
+        }
     }
 }
